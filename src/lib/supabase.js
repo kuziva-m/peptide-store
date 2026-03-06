27f345f6202c280eff1cd766c0afa1404e8d60b4
Kuziva Mawoyo <Kuziva.Mawoyo@outlook.com>
2026-03-06 08:54:52 +0200

    06/03/26-2

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // These lines look for the keys in your .env file
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // <--- FIXED THIS LINE!
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
